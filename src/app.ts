d5582057080759fd6a32e7f3f01efb96353fa0ca
Ouchuxuan <ocx7793709@hotmail>
2021-01-06 21:50:59 +0800

    通过OBS测试推流

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,66 @@ import appRouter from './routes'
 import loggerMiddleware from './middlewares/log';
 import { createConnection } from "typeorm";
 import config from './config'
+import NodeMediaServer from 'node-media-server';
+
+
+const mediaServerConfig = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*'
+    }
+};
+const nms = new NodeMediaServer(mediaServerConfig)
+nms.run();
+// rtmp://192.168.0.10/live/stream
+nms.on('preConnect', (id, args) => {
+    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+    // let session = nms.getSession(id);
+    // session.reject();
+});
+
+nms.on('postConnect', (id, args) => {
+    console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+nms.on('doneConnect', (id, args) => {
+    console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+nms.on('prePublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    // let session = nms.getSession(id);
+    // session.reject();
+});
+
+nms.on('postPublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('donePublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('prePlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    // let session = nms.getSession(id);
+    // session.reject();
+});
+
+nms.on('postPlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('donePlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
 
 const app = new Koa();
 
@@ -43,7 +103,7 @@ app.use(async (ctx, next) => {
 // router
 appRouter.forEach(router => {
     app.use(router.routes()).use(router.allowedMethods())
-  })
+})
 
 app.listen(config.port)
 console.log(`server is listening at port ${config.port}`)
